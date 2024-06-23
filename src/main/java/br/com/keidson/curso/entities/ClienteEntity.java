6041e15d009b414cfc56753310d92e4fc5dbacf5
Keidson Roby <keidsondesigner@gmail.com>
2024-06-23 00:01:20 -0400

    feat: Criado Class Cliente e mapeado campos da tabela Cliente

---
+++ src/main/java/br/com/keidson/curso/entities/ClienteEntity.java
@@ -0,0 +1,9 @@
+package br.com.keidson.curso.entities;
+
+
+public class ClienteEntity {
+    private Long id;
+    private String nome;
+    private EndrecoEntity endreco;
+    private ContatoEntity contato;
+}
