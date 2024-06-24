246e51918ef77dae1e18e072c9b7c276b37999cd
Keidson Roby <keidsondesigner@gmail.com>
2024-06-23 20:21:55 -0400

    feat: Criado mapeamento da tabela tb_cliente

--- src/main/java/br/com/keidson/curso/entities/ClienteEntity.java
+++ src/main/java/br/com/keidson/curso/entities/ClienteEntity.java
@@ -1,9 +1,43 @@
 package br.com.keidson.curso.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode.Include;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-public class ClienteEntity {
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "tb_cliente")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ClienteEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Include
+    @Id
     private Long id;
+    
     private String nome;
-    private EndrecoEntity endreco;
+    private EnderecoEntity endereco;
     private ContatoEntity contato;
+
+    @CreationTimestamp
+    @Column(name = "data_criacao")
+    private ZonedDateTime dataCriacao;
+
+    @UpdateTimestamp
+    @Column(name = "data_atualizacao")
+    private ZonedDateTime dataAtualizacao;
 }
