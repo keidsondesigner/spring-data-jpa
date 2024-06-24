ebacfe11c86cd3c1031d525e19c99a8f7db0b770
Keidson Roby <keidsondesigner@gmail.com>
2024-06-23 20:22:23 -0400

    feat: criado mapeamento da tabela tb_contato

--- src/main/java/br/com/keidson/curso/entities/ContatoEntity.java
+++ src/main/java/br/com/keidson/curso/entities/ContatoEntity.java
@@ -1,8 +1,30 @@
 package br.com.keidson.curso.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode.Include;
 
-public class ContatoEntity {
+import java.io.Serializable;
+
+@Entity // quando uso a anotacão @Entity, preciso usar o @Id
+@Table(name = "tb_contato")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ContatoEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Include
+    @Id
     private Long id;
+    
     private String descricao;
     private String telefone;
 }
