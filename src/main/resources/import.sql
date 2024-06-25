fd1d1e0d33a1d265f37d79b61e647b4a66931eab
Keidson Roby <keidsondesigner@gmail.com>
2024-06-24 23:18:17 -0400

    feat: Adicionado arquivo de criação de Massa de Dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,18 @@
+INSERT INTO tb_contato (descricao, telefone) VALUES ('Contato Principal', '(11) 99999-9999');
+INSERT INTO tb_contato (descricao, telefone) VALUES ('Contato Principal', '(11) 88888-8888');
+INSERT INTO tb_contato (descricao, telefone) VALUES ('Contato Principal', '(11) 77777-7777');
+INSERT INTO tb_contato (descricao, telefone) VALUES ('Contato Principal', '(11) 77777-7776');
+
+  
+  
+
+INSERT INTO tb_endereco (logradouro, cep, bairro) VALUES ('Rua das Flores', '12345-678', 'Centro');
+INSERT INTO tb_endereco (logradouro, cep, bairro) VALUES ('Avenida Paulista', '98765-432', 'Bela Vista');
+INSERT INTO tb_endereco (logradouro, cep, bairro) VALUES ('Praça da Sé', '11111-222', 'Centro');
+INSERT INTO tb_endereco (logradouro, cep, bairro) VALUES ('Rua Conde de Itgua', '690585-82', 'Cidade Nova');
+
+
+INSERT INTO tb_cliente (nome, endereco_id, contato_id, data_criacao) VALUES ('João Silva 1', 1, 1, '2023-10-22');
+INSERT INTO tb_cliente (nome, endereco_id, contato_id, data_criacao) VALUES ('Maria Oliveira 2', 1, 2, '2023-10-22');
+INSERT INTO tb_cliente (nome, endereco_id, contato_id, data_criacao) VALUES ('Carlos Pereira 3', 2, 3, '2023-10-22');
+INSERT INTO tb_cliente (nome, endereco_id, contato_id, data_criacao) VALUES ('Keidson Roby 4', 3, 4, '2023-10-22');
